c7c7a3f3d6d7a9c63fa6fbb73d4f2f182e0bc3c2
Yannick van der Wende <Yannick@rockstars.nl>
2017-05-08 10:56:57 +0200

    was nog wat vergetem

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'sourtree comment
+        'sourtree comment Weet je wat ik was nog wat vergeten
     End Sub
 End Class
